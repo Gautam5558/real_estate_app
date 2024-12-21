1a873b66ab218f0fab434f3dbae27cd6629da18b
Gautam Jain <gautamjain5558@gmail.com>
2024-12-22 00:05:00 +0530

    minor deployment issue fix

--- api/index.js
+++ api/index.js
@@ -10,8 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:
-      "https://real-estate-app-client-dr7yy6kp9-gautam-s-projects-aaff952d.vercel.app",
+    origin: "https://real-estate-app-client.vercel.app/",
     credentials: true,
   })
 );
